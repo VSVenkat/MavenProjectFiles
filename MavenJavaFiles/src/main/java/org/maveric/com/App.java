1edd5b29c2e4e485436dff95573200b4fb0a3018
U-MAV\venkatramanar <venkatramanar@MDLT028.Mav.Est>
2016-02-05 15:37:43 +0530

    Changed App.java

--- MavenJavaFiles/src/main/java/org/maveric/com/App.java
+++ MavenJavaFiles/src/main/java/org/maveric/com/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
 
-        System.out.println("This is maven test");
+        System.out.println("This is maven test for git and jenkins");
 		System.out.println( "Hello World!" );
     }
 
